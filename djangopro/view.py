0374b6568d96d4e74a312dda618398cdba0cafdc
Mrinalsharma1 <checkkro1@gmail.com>
2022-04-25 22:25:27 +0530

    hello

---
+++ djangopro/view.py
@@ -0,0 +1,7 @@
+# This is created my my Own
+from django.http import HttpResponse
+def index(request):
+    return HttpResponse("<h1>Hello django</h1>")
+
+def about(request):
+    return HttpResponse('hello This is About Page')
